3e78757fb35d789acaa960c38bb0ad2fd464cca6
RRequeijo <al79138@alunos.utad.pt>
2025-07-18 22:24:42 +0100

    HABE-53 entitybase and valueobject again 2

--- src/main/java/com/hustleaddiction/chat/chat_api/domain/SeedWork/EntityBase.java
+++ src/main/java/com/hustleaddiction/chat/chat_api/domain/SeedWork/EntityBase.java
@@ -1,45 +1,52 @@
 package com.hustleaddiction.chat.chat_api.domain.SeedWork;
 
 import java.util.Date;
-import java.util.Objects;
 import java.util.UUID;
 
-public abstract class EntityBase {
+public abstract class EntityBase
+{
     private Date creationDate;
     private long id;
     private Date modificationDate;
     private UUID uuid;
 
-    protected EntityBase() {
+    protected EntityBase()
+    {
         this.uuid = UUID.randomUUID();
     }
 
-    // Getters and Setters
-    public Date getCreationDate() {
+    public Date getCreationDate()
+    {
         return creationDate;
     }
 
-    public void setCreationDate(Date creationDate) {
+    public void setCreationDate(Date creationDate)
+    {
         this.creationDate = creationDate;
     }
 
-    public long getId() {
+    public long getId()
+    {
         return id;
     }
 
-    public void setId(long id) {
+    public void setId(long id)
+    {
         this.id = id;
     }
 
-    public Date getModificationDate() {
+    public Date getModificationDate()
+    {
         return modificationDate;
     }
 
-    public void setModificationDate(Date modificationDate) {
+    public void setModificationDate(Date modificationDate)
+    {
         this.modificationDate = modificationDate;
     }
 
-    public UUID getUuid() {
+    public UUID getUuid()
+    {
         return uuid;
     }
 
